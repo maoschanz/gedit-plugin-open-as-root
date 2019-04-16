7a7ed178dbcff92d31613985884f1273ddb4a494
Romain F. T <rrroschan@gmail.com>
2019-04-16 03:33:18 +0200

    Update the item only when it's pertinent

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.local/share/gedit" ]; then
-	mkdir ~/.local/share/gedit
-fi
-if [ ! -d "$HOME/.local/share/gedit/plugins" ]; then
-	mkdir ~/.local/share/gedit/plugins
+if (( $EUID == 0 )); then
+	INSTALL_DIR="/usr/lib/x86_64-linux-gnu/gedit/plugins/"
+else
+	INSTALL_DIR="$HOME/.local/share/gedit/plugins"
 fi
 
-cp open_as_root.plugin ~/.local/share/gedit/plugins/open_as_root.plugin
-cp open_as_root.py ~/.local/share/gedit/plugins/open_as_root.py
+mkdir -p $INSTALL_DIR
 
-#sudo cp markdown_preview.py /usr/lib/x86_64-linux-gnu/gedit/plugins/open_as_root.py
-#sudo cp markdown_preview.plugin /usr/lib/x86_64-linux-gnu/gedit/plugins/open_as_root.plugin
+echo "Installing extension files in $INSTALL_DIR"
+cp open_as_root.plugin $INSTALL_DIR/open_as_root.plugin
+cp open_as_root.py $INSTALL_DIR/open_as_root.py
+echo "Done."
+exit 0
 
--- open_as_root.py
+++ open_as_root.py
@@ -1,3 +1,4 @@
+# GPL v3
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -6,57 +7,54 @@ from gi.repository import GObject, Gtk, Gedit, Gio
 class SaveAsRootAppActivatable(GObject.Object, Gedit.AppActivatable):
 	app = GObject.property(type=Gedit.App)
 	__gtype_name__ = "SaveAsRootAppActivatable"
-
+	
 	def __init__(self):
 		GObject.Object.__init__(self)
-
+	
 	def do_activate(self):
-		self._build_menu()
-
+		self.menu_ext = self.extend_menu('file-section')
+		self.menu_item = Gio.MenuItem.new(_("Edit as administrator…"), 'win.open_as_root')
+		self.menu_ext.append_menu_item(self.menu_item)
+	
 	def do_deactivate(self):
 		self.menu_item = None
 		self.menu_ext = None
 
-	def _build_menu(self):
-		self.menu_ext = self.extend_menu("file-section")
-#		self.menu_ext = self.extend_menu("file-section-1")
-		self.menu_item = Gio.MenuItem.new("Open as root", 'win.open_as_root')
-		self.menu_ext.append_menu_item(self.menu_item)
-
-
 class OpenAsRootWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 	window = GObject.property(type=Gedit.Window)
 	__gtype_name__ = "OpenAsRootWindowActivatable"
-
+	
 	def __init__(self):
 		GObject.Object.__init__(self)
-
+	
 	def do_activate(self):
 		# Defining the action which was set earlier in AppActivatable.
-		self._connect_menu()
-		
-	def do_deactivate(self):
-		pass
-	
-	#this is called every time the gui is updated # FIXME which is stupid and under-optimal
-	def do_update_state(self):
-		# if there is no document in sight, we disable the action, so we don't get NoneException
-		if self.window.get_active_document() is None or self.window.get_active_document().get_location() is None:
-			self.window.lookup_action('open_as_root').set_enabled(False)
-		# if the document is already opened as root, we disable the action too
-		elif 'admin' in self.window.get_active_document().get_location().get_uri_scheme():
-			self.window.lookup_action('open_as_root').set_enabled(False)
-		else:
-			self.window.lookup_action('open_as_root').set_enabled(True)
-
-	def _connect_menu(self):
 		action = Gio.SimpleAction(name='open_as_root')
 		action.connect('activate', self.action_cb)
 		self.window.add_action(action)
-
+		
+		self.window.connect('active-tab-changed', self.update_item_state)
+		self.window.connect('active-tab-state-changed', self.update_item_state)
+	
+	def do_deactivate(self):
+		pass
+	
+	def do_update_state(self):
+		pass
+	
+	def update_item_state(self, *args):
+		if self.window.get_active_document() is None or self.window.get_active_document().get_location() is None:
+			# if there is no document, we disable the action, so we don't get NoneException
+			self.window.lookup_action('open_as_root').set_enabled(False)
+		elif 'admin' in self.window.get_active_document().get_location().get_uri_scheme():
+			# if the document is already opened as root, we disable the action too
+			self.window.lookup_action('open_as_root').set_enabled(False)
+		else:
+			self.window.lookup_action('open_as_root').set_enabled(True)
+	
 	def action_cb(self, action, data):
 		doc = self.window.get_active_document()
-		my_uri = "admin://" + doc.get_uri_for_display()
+		my_uri = 'admin://' + doc.get_uri_for_display()
 		self.window.create_tab_from_location(Gio.File.new_for_uri(my_uri), None, 0, 0, False, True)
 
 
